1714a9694f8da8cea19a933d89ab4e0a32e45eba
Class01 <Class01@DESKTOP-JIAVR8N>
2023-04-26 17:31:58 +0900

    answer

--- 20230426/src/q510/Firstclass07.java
+++ 20230426/src/q510/Firstclass07.java
@@ -2,8 +2,10 @@ package q510;
 
 public class Firstclass07 {
 	public static void main(String[] args) {
-		int a = 55;
-		int b = 2008;
-		System.out.println();
+		int a = 55-10;
+		int b = 2008-1999;
+		
+		System.out.println("55 - 10 = " +a);
+		System.out.println("2008 - 1999 = " +b);
 	}
 }
